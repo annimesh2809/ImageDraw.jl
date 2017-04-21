2be1c82e2f67687336773a084ec92ea3512fcede
Animesh <annimesh2809@gmail.com>
2017-04-21 22:50:51 +0530

    Testing still

--- test/circle2d.jl
+++ test/circle2d.jl
@@ -13,16 +13,16 @@
                                 0 0 1 1 1 1 1 0 0 0
                                 0 0 0 0 0 0 0 0 0 0 ]
 
-        res = draw(img, CirclePointRadius(Point(5,5), 5))
+        res = @inferred draw(img, CirclePointRadius(Point(5,5), 5))
         @test all(expected .== res) == true
 
-        res = draw(img, CirclePointRadius(5, 5, 5))
+        res = @inferred draw(img, CirclePointRadius(5, 5, 5))
         @test all(expected .== res) == true
 
-        res = draw(img, CirclePointRadius(CartesianIndex(5,5), 5))
+        res = @inferred draw(img, CirclePointRadius(CartesianIndex(5,5), 5))
         @test all(expected .== res) == true
 
-        res = draw(img, CirclePointRadius(Point(5,5), 5), Gray{N0f8}(0.5))
+        res = @inferred draw(img, CirclePointRadius(Point(5,5), 5), Gray{N0f8}(0.5))
         expected = map(i->(i==1 ? Gray{N0f8}(0.5) : Gray{N0f8}(0)), expected)
         @test all(expected .== res) == true
 
@@ -42,18 +42,21 @@
                                0 0 1 1 1 1 1 1 1 0
                                0 0 0 1 1 1 1 1 0 0 ]
 
-        res = draw(img, CircleThreePoints(Point(1,5), Point(5,1), Point(10,5)))
+        res = @inferred draw(img, CircleThreePoints(Point(1,5), Point(5,1), Point(10,5)))
         @test all(expected .== res)
 
-        res = draw(img, CircleThreePoints(1,5, 5,1, 10,5))
+        res = @inferred draw(img, CircleThreePoints(1,5, 5,1, 10,5))
         @test all(expected .== res)
 
-        res = draw(img, CircleThreePoints(CartesianIndex(5,1), CartesianIndex(1,5), CartesianIndex(5,10)))
+        res = @inferred draw(img, CircleThreePoints(CartesianIndex(5,1), CartesianIndex(1,5), CartesianIndex(5,10)))
         @test all(expected .== res)
 
-        res = draw(img, CircleThreePoints(Point(1,5), Point(5,1), Point(10,5)), Gray{N0f8}(0.5))
+        res = @inferred draw(img, CircleThreePoints(Point(1,5), Point(5,1), Point(10,5)), Gray{N0f8}(0.5))
         expected = map(i->(i==1 ? Gray{N0f8}(0.5) : Gray{N0f8}(0)), expected)
         @test all(expected .== res) == true
+
+        invalid_circle = CircleThreePoints(Point(1,1), Point(2,2), Point(3,3))
+        @test_throws ErrorException draw(img, invalid_circle)
 
     end
 end
